f5c757fd268b0b35cb07d7329e1ebf36b601af6f
bmeistergeneral <brecon.morgan@gmail.com>
2024-10-04 13:20:18 -0400

    Created Users table

--- setup.sql
+++ setup.sql
@@ -1,2 +1,12 @@
 Create database passwords;
 
+use passwords;
+
+CREATE TABLE Users (
+user_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+user_first_name varchar(32) NOT NULL,
+user_last_name varchar(32) NOT NULL,
+user_username varchar(32) NOT NULL,
+user_email varchar(40) NOT NULL,
+PRIMARY KEY (user_id)
+);
