8b8a1142375930f008e1a9e9200d1f87f7822982
bmeistergeneral <brecon.morgan@gmail.com>
2024-10-04 13:21:38 -0400

    Created Websites table

--- setup.sql
+++ setup.sql
@@ -10,3 +10,13 @@ user_username varchar(32) NOT NULL,
 user_email varchar(40) NOT NULL,
 PRIMARY KEY (user_id)
 );
+
+CREATE TABLE Websites (
+website_name varchar(255) NOT NULL,
+website_url varchar(1000) NOT NULL,
+user_id MEDIUMINT NOT NULL,
+password varchar(40) NOT NULL,
+comment varchar(255) NOT NULL,
+create_time TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
