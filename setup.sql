a26bdbd52aad07e209a0be2c6df69497ca122a65
bmeistergeneral <brecon.morgan@gmail.com>
2024-10-04 13:26:27 -0400

    Changed to use singular table names, seems like a better idea

--- setup.sql
+++ setup.sql
@@ -2,7 +2,7 @@ Create database passwords;
 
 use passwords;
 
-CREATE TABLE Users (
+CREATE TABLE User (
 user_id MEDIUMINT NOT NULL AUTO_INCREMENT,
 user_first_name varchar(32) NOT NULL,
 user_last_name varchar(32) NOT NULL,
@@ -11,12 +11,12 @@ user_email varchar(40) NOT NULL,
 PRIMARY KEY (user_id)
 );
 
-CREATE TABLE Websites (
+CREATE TABLE Website (
 website_name varchar(255) NOT NULL,
 website_url varchar(1000) NOT NULL,
 user_id MEDIUMINT NOT NULL,
 password varchar(40) NOT NULL,
 comment varchar(255) NOT NULL,
 create_time TIMESTAMP,
-FOREIGN KEY (user_id) REFERENCES Users(user_id)
+FOREIGN KEY (user_id) REFERENCES User(user_id)
 );
