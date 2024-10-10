23600bff695a1989cda5982f0f1ed9b9494d8cdb
bmeistergeneral <brecon.morgan@gmail.com>
2024-10-10 16:51:06 -0400

    Created encryption of passwords using AES_Encrypt

--- setup.sql
+++ setup.sql
@@ -15,7 +15,7 @@ CREATE TABLE Passwords (
 website_name varchar(255) NOT NULL,
 website_url varchar(1000) NOT NULL,
 user_id MEDIUMINT NOT NULL,
-password varchar(40) NOT NULL,
+password varbinary(512) NOT NULL,
 comment varchar(255),
 create_time TIMESTAMP,
 FOREIGN KEY (user_id) REFERENCES Users(user_id)
@@ -30,16 +30,20 @@ values
 ("John", "Smith", "js780d", "johnsmith@icloud.com"),
 ("Draco", "Malfoy", "severusrocks", "dracosucks@yahoo.com");
 
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('ThisKitchenIsForDancing', 512));
+SET @init_vector = RANDOM_BYTES(16);
+
 -- Don't want to manually set user_id, but since it's initial data I think it is ok.
 insert into Passwords (website_name, website_url, user_id, password, comment)
 values
-("Atlassian", "https://www.atlassian.com", 1, 'VknsS>(LR#,G5d)8FA;jJ@', NULL),
-("JetBrains", "https://www.jetbrains.com", 1, 'qp+6N8v754yK_PF/*HauBR', NULL),
-("MySQL", "https://www.mysql.com", 1, 'C<@3a%ws9/TWe#`c$8}-7,', NULL),
-("AMC", "https://www.amctheatres.com", 1, 'f^j2@!NJ8)(m[:5&*4Kk=Y', "movie tickets"),
-("Google", "https://www.google.com", 2, 'Ef649#2P=r5%U+3Mh$eZq/', NULL),
-("Ableton", "https://www.ableton.com", 2, 't~Gv&Z`4?Ac6FjqbJM@mK^', NULL),
-("Netflix", "https://www.netflix.com", 2, 'f+YLu{,@T")3}4N8c<wHr8', NULL),
-("NordVPN", "https://www.nordvpn.com", 3, 'ps(6[S82DFEMg;ZTNCa+k=', NULL),
-("Spotify", "https://open.spotify.com", 3, 'XVKW5$eG=h6s?mdL8x&bQ', "student account"),
-("Pearson", "https://www.pearson.com/en-us.html", 3, 'Jv%{yX?35p9s,kV]TB8!"^', NULL);
+("Atlassian", "https://www.atlassian.com", 1, AES_ENCRYPT('VknsS>(LR#,G5d)8FA;jJ@', @key_str, @init_vector), NULL),
+("JetBrains", "https://www.jetbrains.com", 1, AES_ENCRYPT('qp+6N8v754yK_PF/*HauBR', @key_str, @init_vector), NULL),
+("MySQL", "https://www.mysql.com", 1, AES_ENCRYPT('C<@3a%ws9/TWe#`c$8}-7,', @key_str, @init_vector), NULL),
+("AMC", "https://www.amctheatres.com", 1, AES_ENCRYPT('f^j2@!NJ8)(m[:5&*4Kk=Y', @key_str, @init_vector), "movie tickets"),
+("Google", "https://www.google.com", 2, AES_ENCRYPT('Ef649#2P=r5%U+3Mh$eZq/', @key_str, @init_vector), NULL),
+("Ableton", "https://www.ableton.com", 2, AES_ENCRYPT('t~Gv&Z`4?Ac6FjqbJM@mK^', @key_str, @init_vector), NULL),
+("Netflix", "https://www.netflix.com", 2, AES_ENCRYPT('f+YLu{,@T")3}4N8c<wHr8', @key_str, @init_vector), NULL),
+("NordVPN", "https://www.nordvpn.com", 3, AES_ENCRYPT('ps(6[S82DFEMg;ZTNCa+k=', @key_str, @init_vector), NULL),
+("Spotify", "https://open.spotify.com", 3, AES_ENCRYPT('XVKW5$eG=h6s?mdL8x&bQ', @key_str, @init_vector), "student account"),
+("Pearson", "https://www.pearson.com/en-us.html", 3, AES_ENCRYPT('Jv%{yX?35p9s,kV]TB8!"^', @key_str, @init_vector), NULL);
